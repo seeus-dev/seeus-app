f4488cb4e671d14fd353c8142ede178920b785db
Nathan Johnson <nathancj224@gmail.com>
2020-03-22 00:48:33 -0400

    Configure ESLint and Prettier with better settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,32 @@
 module.exports = {
-    extends: "airbnb-typescript-prettier",
+    parser: "@typescript-eslint/parser",
+    extends: [
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    env: {
+        es6: true,
+        browser: true,
+        node: true,
+    },
     rules: {
-        "object-curly-spacing": [2, "always"],
-        "prettier/prettier": "warn"
-    }
+        'prettier/prettier': 'warn',
+        'no-unused-vars': 'warn',
+        'object-curly-spacing': [2, 'always'],
+        '@typescript-eslint/no-use-before-define': 'off',
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
 };
