8c6b2ed26e6ac90107cc71c019cfc49fc2606ca6
MMarie <mdomas1@emich.edu>
2020-03-25 18:21:16 -0400

    Update HelpScreen

--- src/screens/HelpScreen.tsx
+++ src/screens/HelpScreen.tsx
@@ -1,14 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+
+import React, {Component} from 'react';
+import { Image, StyleSheet, Text, View, Linking, Platform } from 'react-native';
 import baseStyle from '../styles/base';
+import PhoneButton from '../components/PhoneButton';
 
 export default function HelpScreen() {
+  function dialCall() {
+  let phoneNumber = '';
+
+  if (Platform.OS === 'android'){
+    phoneNumber = 'tel:${7344873387}';
+  }
+  else{
+    phoneNumber = 'telprompt:${7344873387}';
+  }
+  Linking.openURL(phoneNumber);
+};
+
   return (
     <View style={styles.container}>
-      <Text>Help Screen</Text>
+    <View style={styles.logoContainer}>
+      <Image
+        style={styles.logo}
+        source={require('../../assets/seeus-logo.png')}
+      />
+      <Text>Contact SEEUS</Text>
+    <PhoneButton
+    //  label="734.487.3387"
+      style={styles.loginButton}
+      labelStyle={styles.loginButtonText}
+      showShadow={true}
+      onPress={() => dialCall()}
+    />
+    </View>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
