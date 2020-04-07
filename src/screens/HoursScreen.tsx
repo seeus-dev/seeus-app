60dde10c0841e8f6f071ccc96e105d6fb25793cf
Riley <ragriffis@gmail.com>
2020-04-07 00:50:22 -0400

    Added SectionList For Hours
    Split SeeUs hours into 3 different categories based on the service. We can add option to set DATA from dispatcher app later

--- src/screens/HoursScreen.tsx
+++ src/screens/HoursScreen.tsx
@@ -1,17 +1,79 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, SectionList} from 'react-native';
 import baseStyle from '../styles/base';
+import colors, { theme } from '../styles/colors';
+import Constants from "expo-constants";
+
+const DATA = [
+  {
+    title: "Request A Walk",
+    data: ["", "", ""]
+  },
+  {
+    title: "Request SeeUs Van",
+    data: ["", "", ""]
+  },
+  {
+    title: "Request A Jump/Unlock",
+    data: ["", "", ""]
+  }
+];
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <SectionList
+      sections={DATA}
+      keyExtractor={(item, index) => item + index}
+      renderItem={({ item }) => <Item title={item} />}
+      renderSectionHeader={({ section: { title } }) => (
+        <Text style={styles.header}>{title}</Text>
+      )}
+    />
+  </SafeAreaView>
+);
+
+export default App;
+
+
 
 export default function HoursScreen() {
   return (
     <View style={styles.container}>
-      <Text>Hours Screen testing for changes</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    ...baseStyle.container,
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+    backgroundColor: theme.primary,
+    paddingTop: 50,
+    paddingBottom: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  item: {
+    backgroundColor: colors.seeusYellow,
+    paddingVertical: 25,
+    paddingHorizontal: 100,
+    marginVertical: 1,
+    alignItems: 'flex-start',
+  },
+  header: {
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+    fontSize: 25,
+    backgroundColor: "#fff",
+    textAlign: 'center',
+  },
+  title: {
+    fontSize: 15
+  }
 });
