0fdc25d24a75b682465af309b2b6a018472d2367
MMarie <mdomas1@emich.edu>
2020-04-06 22:48:06 -0400

    Add report form link to report button

--- src/screens/HelpScreen.tsx
+++ src/screens/HelpScreen.tsx
@@ -1,19 +1,11 @@
 import React, { ReactChild } from 'react';
-import {
-  Image,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { Linking, Image, StyleSheet, Text, View } from 'react-native';
 import baseStyle from '../styles/base';
 import PhoneButton from '../components/PhoneButton';
 import Button from '../components/Button';
-import { Feather } from "@expo/vector-icons";
-
-
+import { Feather } from '@expo/vector-icons';
 
 export default function HelpScreen() {
-
   return (
     <View style={styles.container}>
       <View style={styles.container}>
@@ -22,18 +14,22 @@ export default function HelpScreen() {
           source={require('../../assets/seeus-logo.png')}
         />
         <Text style={styles.text}>Contact SEEUS</Text>
-         <PhoneButton/>
-         <Text style={styles.text2}>Found a problem with the SEEUS App?</Text>
-         <Button
-           label="Report Bug  "
-           style={styles.reportButton}
-           labelStyle={styles.labelR}
-           showShadow={true}
-          // onPress={}
-         >
-         <Feather  name="alert-circle" size={25} />
-         </Button>
-         <Text style={styles.text3}>Version: 0.1</Text>
+        <PhoneButton />
+        <Text style={styles.text2}>Found a problem with the SEEUS App?</Text>
+        <Button
+          label="Report Bug  "
+          style={styles.reportButton}
+          labelStyle={styles.labelR}
+          showShadow={true}
+          onPress={() => {
+            Linking.openURL(
+              'https://docs.google.com/forms/d/e/1FAIpQLSeNdiG3tpVvTMvutglPaXo-ojCMCATDV11MvyzDsGtwxItTiQ/viewform?usp=sf_link'
+            );
+          }}
+        >
+          <Feather name="alert-circle" size={25} />
+        </Button>
+        <Text style={styles.text3}>Version: 0.1</Text>
       </View>
     </View>
   );
@@ -49,43 +45,37 @@ const styles = StyleSheet.create({
   },
   logo: {
     paddingTop: 100,
-    width:125,
+    width: 125,
     height: 125,
-    shadowOffset: { width: .5, height: .5 },
+    shadowOffset: { width: 0.5, height: 0.5 },
     shadowColor: '#000',
     shadowOpacity: 0.5,
-  //  elevation: 10,
-
+    //  elevation: 10,
   },
 
-  text:
-  {
+  text: {
     fontSize: 40,
     fontWeight: 'bold',
     paddingTop: 10,
     paddingBottom: 1,
   },
 
-  text2:
-  {
+  text2: {
     fontSize: 20,
     paddingTop: 200,
     fontWeight: 'bold',
   },
 
-   reportButton:
-  {
+  reportButton: {
     paddingBottom: 15,
   },
 
-  labelR:
-  {
+  labelR: {
     fontSize: 28,
     fontWeight: 'bold',
   },
-  text3:
-  {
+  text3: {
     fontSize: 20,
-  color: 'gray'
+    color: 'gray',
   },
 });
