5a4fef8e7d803719f449938a16c6733becaf0e99
Riley <ragriffis@gmail.com>
2020-04-09 11:13:33 -0400

    Update HoursScreen.tsx

--- src/screens/HoursScreen.tsx
+++ src/screens/HoursScreen.tsx
@@ -8,23 +8,35 @@ const DATA = [
   {
     title: "Request A Walk",
     data:
-    ["Sun                                 5:00pm-3:00am",
-     "Mon                                 5:00pm-1:00am",
-     "Tue                                 5:00pm-1:00am",
-     "Wed                                 5:00pm-1:00am",
-     "Thu                                 5:00pm-1:00am",
-     "Fri                                 5:00pm-1:00am",
-     "Sat                                 5:00pm-1:00am",
+    ['Sun                                 5:00pm-3:00am',
+     'Mon                                 5:00pm-1:00am',
+     'Tue                                 5:00pm-1:00am',
+     'Wed                                 5:00pm-1:00am',
+     'Thu                                 5:00pm-1:00am',
+     'Fri                                 5:00pm-1:00am',
+     'Sat                                 5:00pm-1:00am',
 
     ]
   },
   {
-    title: "Request SeeUs Van",
-    data: ["", "", "","",""]
+    title: 'Request SeeUs Van',
+    data: ['Sun                                 5:00pm-3:00am',
+     'Mon                                 5:00pm-1:00am',
+     'Tue                                 5:00pm-1:00am',
+     'Wed                                 5:00pm-1:00am',
+     'Thu                                 5:00pm-1:00am',
+     'Fri                                 5:00pm-1:00am',
+     'Sat                                 5:00pm-1:00am',]
   },
   {
-    title: "Request A Jump/Unlock",
-    data: ["", "", "","",""]
+    title: 'Request A Jump/Unlock',
+    data: ['Sun                                 5:00pm-3:00am',
+     'Mon                                 5:00pm-1:00am',
+     'Tue                                 5:00pm-1:00am',
+     'Wed                                 5:00pm-1:00am',
+     'Thu                                 5:00pm-1:00am',
+     'Fri                                 5:00pm-1:00am',
+     'Sat                                 5:00pm-1:00am',]
   }
 ];
 
