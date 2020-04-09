48c18c886a19d8824195fe6409e711c5b29994ef
Nathan Johnson <n.johnson@criteo.com>
2020-04-09 09:08:01 -0400

    Add fake user agent to webview so Google doesn't reject it

--- src/screens/login/OauthWebViewScreen.tsx
+++ src/screens/login/OauthWebViewScreen.tsx
@@ -18,6 +18,11 @@ type ScreenProps = {
   navigation: StackNavigationProp<any>;
 };
 
+const WEB_VIEW_USER_AGENT =
+  'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) ' +
+  'AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 ' +
+  'Safari/602.1';
+
 export default function OauthWebViewScreen(props: ScreenProps) {
   const { route, navigation } = props;
   const { username } = route.params;
@@ -73,6 +78,7 @@ export default function OauthWebViewScreen(props: ScreenProps) {
           onLoadEnd={() => setLoading(false)}
           onMessage={onWebViewMessage}
           accessibilityHint="emich.edu login"
+          userAgent={WEB_VIEW_USER_AGENT}
         />
       </View>
     </View>
